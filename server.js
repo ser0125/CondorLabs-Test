ae525aa7cb1ae0e848363be51a4490282e65ee06
ser0125 <sergio.llanos.garcia@gmail.com>
2017-10-29 22:23:26 -0500

    Added mongoose to the project

--- server.js
+++ server.js
@@ -1,95 +1,107 @@
-//We are going to use express and Mongodb so here we import all of them
+//We are going to use express and Moongose so here we import all of them
 const express = require('express');
 const bodyParser = require('body-parser');
-const MongoClient = require('mongodb').MongoClient;
-
+const mongoose = require('mongoose');  
+var db
 
 const app = express()
 app.use(bodyParser.json())
 
 //Here we connect to the database
 var db
-MongoClient.connect('mongodb://sergioll:sergioll@ds235775.mlab.com:35775/evercheck-test-sergio-llanos', (err, database) => {
+mongoose.connect('mongodb://sergioll:sergioll@ds235775.mlab.com:35775/evercheck-test-sergio-llanos', (err, database) => {
   if (err) return console.log(err)
   db = database
   app.listen(3000, () => {
     console.log('listening on 3000')
   })
 })
+
+var Schema = mongoose.Schema; 
+
+var providerSchema = new Schema({
+firstName : String,
+lastName : String,
+middleName : String,
+email : String,
+ specialty : {
+  name: String,
+  createdBy: Number,
+  createdAt: Date,
+  updatedBy: Number,
+  updatedAt: Date
+},
+projectedStartDate: Number,
+employerId: Number,
+providerType: String,
+staffStatus: String,
+assignedTo: Number,
+status: String,
+createdBy: Number,
+createdAt: Date,
+updatedBy: Number,
+updatedAt: Date
+})
+
+var providers = mongoose.model("Providers",providerSchema);
+
 //This method return all the data from providers
 app.get('/list', (req, res) => {
-  //.find() return a cursor and this have a method that is toArray that return the data
-  var cursor = db.collection('providers').find().toArray(function (err, results) {
+  providers.find(function(err, results) {
     res.send(results)
-    
   })
 })
 //This method return all the data with the firstName like a param 
 app.get('/list/:firstName', (req, res) => {
-  //.find() return a cursor and this have a method that is toArray that return the data
-  var query = {
-    firstName: req.params.firstName
-  }
-  var cursor = db.collection('providers').find(query).toArray(function (err, results) {
+  providers.find({firstName : req.params.firstName},function(err, results) {
     res.send(results)
   })
 })
+
 //this method save in to the database
 app.post('/saveInfo', (req, res) => {
-  db.collection('providers').save(req.body, (err, result) => {
-    if (err) return console.log(err)
-    console.log('saved to database')
-    res.redirect('/list')
+  var provider = new providers({
+    firstName: req.body.firstName,
+    lastName :req.body.lastName,
+    middleName : req.body.middleName,
+    email : req.body.email,
+  });
+  provider.save(function() {
+      res.send("Your data is saved");
+    })
   })
-})
+
 //this method actualize the first object from the collection that match with the name
-app.put('/updateInfo/:firstName', (req, res) => {
-  var userToUpdate = req.params.firstName
-  db.collection('providers').updateOne({
-    firstName: userToUpdate
-  }, req.body, (err, result) => {
-    if (err) return console.log(err)
-    console.log('Update your changes')
-
+  app.put('/updateInfo/:firstName', (req, res) => {
+    providers.findOne({firstName: req.params.firstName}, (err, provider) => {
+      if (err)  return handleError(err)
+      provider.firstName = req.body.firstName
+      provider.save(function() {
+        res.send("Your data is actualized");
+      })
+    })
   })
-})
-//this method actualize the first name of all the objects that match with the name in the collection
-app.put('/updateAllInfo/:firstName', (req, res) => {
-  var usersToUpdate = req.params.firstName
-  var newvalues = {
-    $set: {
-      firstName: req.body.firstName
-    }
-  };
-  db.collection('providers').updateMany({
-    firstName: usersToUpdate
-  }, newvalues, (err, result) => {
-    if (err) return console.log(err)
-    console.log('Update your changes')
+  //this method actualize all the objects from the collection from the collection that match with the name
+  app.put('/updateAllInfo/:firstName', (req, res) => {
+    providers.updateMany({firstName: req.params.firstName},{ $set: { firstName: req.body.firstName }},{ multi: true },(err, provider) => {
+      if (err)  return handleError(err)
+        res.send("Your data is actualized");
+    })
   })
-})
 
+//This method delete the first object from the collection that match with the name
 app.delete('/deleteInfo/:firstName', (req, res) => {
-  var userToEliminate = req.params.firstName
-  db.collection('providers').deleteOne({
-    firstName: userToEliminate
-  }, req.body, (err, result) => {
-    if (err) return console.log(err)
-    console.log('The user is deleted')
-    db.close()
+  providers.findOneAndRemove({firstName: req.params.firstName}, (err) =>{
+    if(!err) {
+      res.send("User was deleted");
+  } 
   })
 })
+//This method delete all the objects from the collection that match with the name
 app.delete('/deleteAllInfo/:firstName', (req, res) => {
-  var usersToDelete = req.params.firstName
-  var newvalues = {
-    $set: {
-      firstName: req.body.firstName
-    }
-  };
-  db.collection('providers').deleteMany({
-    firstName: usersToDelete
-  }, newvalues, (err, result) => {
-    if (err) return console.log(err)
-    console.log('The user is deleted')
+  providers.deleteMany({firstName: req.params.firstName}, (err) =>{
+    if(!err) {
+      res.send("Users with that name are deleted");
+  } 
   })
 })
